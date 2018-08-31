09a22f6dd55c4fcdbb152abc8f9b40da22db9164
EDMPL <akaedmpl@gmail.com>
2018-08-31 12:05:45 +0700

    Fix Form Login dan Action Bar Color

--- AplikasiPuskesmas/app/src/main/java/com/example/akaed/aplikasipuskesmas/activity/LoginActivity.java
+++ AplikasiPuskesmas/app/src/main/java/com/example/akaed/aplikasipuskesmas/activity/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.akaed.aplikasipuskesmas.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,7 +24,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         username = (EditText) findViewById(R.id.username);
+        username.setInputType(InputType.TYPE_CLASS_TEXT);
+
         password = (EditText) findViewById(R.id.password);
+
         loginBtn = (Button) findViewById(R.id.login);
         registerButton = (Button) findViewById(R.id.register);
 
